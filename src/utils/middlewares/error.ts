d17489ff74bd2c43abdaa91b3265e29223dc40a5
guntar <guntar@me.com>
2024-04-12 09:29:16 +0700

    first commit

---
+++ src/utils/middlewares/error.ts
@@ -0,0 +1,16 @@
+import { NextFunction, Request, Response } from 'express';
+import BaseError from '../errors/base-error';
+
+export const errorMiddleware = (error: BaseError, req: Request, res: Response, _: NextFunction) => {
+  const message = error.message || 'Something went wrong';
+  const errorResponse: Record<string, string> = {
+    message
+  }
+
+  if (error.stack && process.env.NODE_ENV !== 'production') {
+    errorResponse["stack"] = error.stack;
+  }
+
+
+  return res.status(error.httpCode).send(errorResponse);
+}
